e14d684e0b62b67a85592996c9d621cb0073a4d4
Welly Shen <hivoid19@gmail.com>
2019-11-14 16:03:59 +0800

    Setup loadImgSize util testing

--- src/utils/__tests__/loadImgSize.test.ts
+++ src/utils/__tests__/loadImgSize.test.ts
@@ -1,15 +1,47 @@
-/* import loadImgSize from '../loadImgSize';
-import loadingImg from '../../App/images/loading.gif';
+import loadImgSize from '../loadImgSize';
 
-it('test', done => {
-  const cb = (error: any, data: { width: string; height: string }): void => {
-    expect(error).toBe(null);
-    expect(data.width).toBe('252');
-    expect(data.height).toBe('252');
-    done();
-  };
+describe('loadImgSize', () => {
+  const WIDTH = 500;
+  const HEIGHT = 500;
+  const SUCCESS_SRC = 'SUCCESS_SRC';
+  const FAILURE_SRC = 'FAILURE_SRC';
 
-  loadImgSize(loadingImg, null, null, cb);
-}); */
+  beforeAll(() => {
+    // Mock Image events
+    // @ts-ignore
+    Object.defineProperties(global.Image.prototype, {
+      naturalWidth: {
+        value: WIDTH
+      },
+      naturalHeight: {
+        value: HEIGHT
+      },
+      src: {
+        set(src): void {
+          if (src === FAILURE_SRC) {
+            setTimeout(() => this.onerror());
+          } else if (src === SUCCESS_SRC) {
+            setTimeout(() => this.onload());
+          }
+        }
+      }
+    });
+  });
 
-it('mock test', () => {});
+  it('image loaded failure, get error message', done => {
+    loadImgSize(FAILURE_SRC, null, null, (error, data) => {
+      expect(error).toBe(`Failed to load ${FAILURE_SRC}`);
+      expect(data).toBeUndefined();
+      done();
+    });
+  });
+
+  it('image loaded success, get the width and height', done => {
+    loadImgSize(SUCCESS_SRC, null, null, (error, data) => {
+      expect(error).toBeNull();
+      expect(data.width).toBe(WIDTH);
+      expect(data.height).toBe(HEIGHT);
+      done();
+    });
+  });
+});
