575ee8b66fcf03b779c142a85bc18a0c687c4aa4
Welly Shen <hivoid19@gmail.com>
2019-11-18 00:10:21 +0800

    Add experiment css file

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -6,6 +6,7 @@ import Img from '../Img';
 import loadingImg from '../static/loading.gif';
 import emptyImg from '../static/empty.svg';
 import { root, app, container } from './styles';
+import './styles.css';
 
 const App: SFC<{}> = () => {
   const [showImg, setShowImg] = useState(true);
@@ -26,13 +27,14 @@ const App: SFC<{}> = () => {
         <div css={container}>
           {showImg && (
             <Img
-              src="https://picsum.photos/500"
+              // className="image"
+              src="https://picsum.photos/252"
               // defaultSrc={loadingImg}
               errorSrc={emptyImg}
               // decode={false}
               // defaultAsError={false}
-              width="250"
-              // height="500"
+              width="252"
+              height="252"
               // onLoad={e => {
               //   console.log('LOG ===> Load: ', e);
               // }}
