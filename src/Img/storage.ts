da569e973b3b76a868b07f5a4e3e04cbd0e6fb40
Welly Shen <hivoid19@gmail.com>
2019-12-07 18:50:24 +0800

    Add session storage util & Add session storage testing

---
+++ src/Img/storage.ts
@@ -0,0 +1,24 @@
+const key = 'REACT_COOL_IMG';
+
+export const set = (src: string): void => {
+  try {
+    const data = JSON.parse(sessionStorage.getItem(key)) || {};
+    data[src] = new Date();
+    sessionStorage.setItem(key, JSON.stringify(data));
+  } catch (error) {
+    /* istanbul ignore next */
+    console.error(`💡react-cool-img: ${error}`);
+  }
+};
+
+export const get = (src: string): boolean => {
+  try {
+    const data = JSON.parse(sessionStorage.getItem(key)) || {};
+    return !!data[src] || false;
+  } catch (error) {
+    /* istanbul ignore next */
+    console.error(`💡react-cool-img: ${error}`);
+    /* istanbul ignore next */
+    return false;
+  }
+};
---
+++ test/storage.test.ts
@@ -0,0 +1,24 @@
+import { set, get } from '../src/Img/storage';
+
+describe('storage', () => {
+  const key = 'REACT_COOL_IMG';
+  const src = 'https://test-image-url';
+
+  it('should set session storage', () => {
+    set(src);
+
+    expect(
+      Object.keys(JSON.parse(window.sessionStorage.getItem(key)))
+    ).toMatchSnapshot();
+  });
+
+  it('should get session storage', () => {
+    window.sessionStorage.removeItem(key);
+
+    expect(get(src)).toBeFalsy();
+
+    set(src);
+
+    expect(get(src)).toBeTruthy();
+  });
+});
