5c887db05d05a5cec7d931794b02f94c973671a0
Welly Shen <hivoid19@gmail.com>
2020-02-17 20:50:02 +0800

    Refactor: adjust file structure

--- src/__tests__/Img.tsx
+++ src/__tests__/Img.tsx
@@ -20,7 +20,7 @@ import { render } from '@testing-library/react';
 
 import useObserver from '../useObserver';
 import * as storage from '../storage';
-import Img from '../Img';
+import Img from '..';
 
 describe('<Img />', () => {
   const props = {
