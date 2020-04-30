99fae194454ae36c361a97ca0a78b125292efb8d
Welly Shen <hivoid19@gmail.com>
2020-04-30 21:06:22 +0800

    test(useObserver): code refactor

--- src/__tests__/useObserver.ts
+++ src/__tests__/useObserver.ts
@@ -7,6 +7,24 @@ import useObserver, {
   Return as Current,
 } from '../useObserver';
 
+interface Args extends Options {
+  debounce?: number;
+}
+type Return = { current: Current };
+
+const renderHelper = ({
+  root = null,
+  rootMargin = '50px',
+  threshold = 0.01,
+  debounce = 300,
+}: Args = {}): Return => {
+  const { result } = renderHook(() =>
+    useObserver(debounce, { root, rootMargin, threshold })
+  );
+
+  return result;
+};
+
 describe('useObserver › errors', () => {
   global.console.error = jest.fn();
   const mockIntersectionObserver = jest.fn((_, { threshold }) => ({
@@ -15,13 +33,13 @@ describe('useObserver › errors', () => {
   }));
 
   it('should handle IntersectionObserver error correctly', () => {
-    renderHook(() => useObserver(300, {}));
+    renderHelper();
 
     expect(console.error).toHaveBeenCalledWith(observerErr);
 
     // @ts-ignore
     global.IntersectionObserver = mockIntersectionObserver;
-    renderHook(() => useObserver(300, {}));
+    renderHelper();
 
     expect(console.error).toHaveBeenCalledWith(observerErr);
   });
@@ -34,7 +52,7 @@ describe('useObserver › errors', () => {
     // @ts-ignore
     global.IntersectionObserverEntry.prototype.isIntersecting = false;
     // @ts-ignore
-    renderHook(() => useObserver(300, { threshold: [0.5, 1] }));
+    renderHelper({ threshold: [0.5, 1] });
 
     expect(console.error).toHaveBeenCalledWith(thresholdErr);
     // @ts-ignore
@@ -47,24 +65,6 @@ describe('useObserver', () => {
 
   const img = document.createElement('img');
   const disconnect = jest.fn();
-
-  interface Args extends Options {
-    debounce?: number;
-  }
-  type Return = { current: Current };
-
-  const testHook = ({
-    root = null,
-    rootMargin = '50px',
-    threshold = 0.01,
-    debounce = 300,
-  }: Args = {}): Return => {
-    const { result } = renderHook(() =>
-      useObserver(debounce, { root, rootMargin, threshold })
-    );
-
-    return result;
-  };
 
   let callback: Function;
   const setIsIntersecting = (isIntersecting: boolean): void => {
@@ -87,7 +87,7 @@ describe('useObserver', () => {
   });
 
   it('should setup intersection observer options correctly', () => {
-    testHook();
+    renderHelper();
 
     // @ts-ignore
     let mkObserver = IntersectionObserver.mock.results[0].value;
@@ -98,7 +98,7 @@ describe('useObserver', () => {
 
     const options = { root: document.body, rootMargin: '100px', threshold: 1 };
 
-    testHook(options);
+    renderHelper(options);
 
     // @ts-ignore
     mkObserver = IntersectionObserver.mock.results[1].value;
@@ -109,7 +109,7 @@ describe('useObserver', () => {
   });
 
   it('should be out-view state', () => {
-    const result = testHook();
+    const result = renderHelper();
     let [setRef, startLoad] = result.current;
 
     act(() => {
@@ -129,7 +129,7 @@ describe('useObserver', () => {
 
   it('should be out-view state due to debounce', () => {
     const debounce = 300;
-    const result = testHook({ debounce });
+    const result = renderHelper({ debounce });
     let [setRef, startLoad] = result.current;
 
     act(() => {
@@ -153,7 +153,7 @@ describe('useObserver', () => {
 
   it('should be in-view state without debounce', () => {
     const debounce = 0;
-    const result = testHook({ debounce });
+    const result = renderHelper({ debounce });
     let [setRef, startLoad, setStartLoad] = result.current;
 
     act(() => {
@@ -174,7 +174,7 @@ describe('useObserver', () => {
   });
 
   it('should be in-view state with debounce', () => {
-    const result = testHook();
+    const result = renderHelper();
     let [setRef, startLoad, setStartLoad] = result.current;
 
     act(() => {
@@ -196,7 +196,7 @@ describe('useObserver', () => {
   });
 
   it('should stop observe when un-mount', () => {
-    testHook();
+    renderHelper();
 
     expect(disconnect).toHaveBeenCalled();
   });
