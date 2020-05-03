d9011bbc2fce72af48779b1b5763d6382e69f467
Welly Shen <hivoid19@gmail.com>
2020-05-03 17:00:45 +0800

    refactor: remove un-necessary false condition & use es2020 syntax

--- src/storage.ts
+++ src/storage.ts
@@ -2,7 +2,7 @@ const key = 'REACT_COOL_IMG';
 
 export const set = (src: string): void => {
   try {
-    const data = JSON.parse(sessionStorage.getItem(key)) || {};
+    const data = JSON.parse(sessionStorage.getItem(key)) ?? {};
     data[src] = new Date();
     sessionStorage.setItem(key, JSON.stringify(data));
   } catch (error) {
@@ -13,8 +13,8 @@ export const set = (src: string): void => {
 
 export const get = (src: string): boolean => {
   try {
-    const data = JSON.parse(sessionStorage.getItem(key)) || {};
-    return !!data[src] || false;
+    const data = JSON.parse(sessionStorage.getItem(key)) ?? {};
+    return !!data[src];
   } catch (error) {
     /* istanbul ignore next */
     console.error(`💡react-cool-img: ${error}`);
