62a552a9327fe6950286aba36cdc7ef5ec8f2a99
Welly Shen <hivoid19@gmail.com>
2020-10-28 00:03:40 +0800

    fix: type errors

--- src/useObserver.ts
+++ src/useObserver.ts
@@ -1,11 +1,4 @@
-import {
-  Dispatch,
-  MutableRefObject,
-  useState,
-  useRef,
-  useEffect,
-  useCallback,
-} from "react";
+import { Dispatch, useState, useRef, useEffect, useCallback } from "react";
 
 export const observerErr =
   "💡react-cool-img: the browser doesn't support Intersection Observer, please install polyfill to enable lazy loading: https://github.com/wellyshen/react-cool-img#intersection-observer-polyfill";
@@ -25,10 +18,8 @@ export default (
 ): Return => {
   const [startLoad, setStartLoad] = useState<boolean>(false);
   const [el, setEl] = useState<HTMLElement | null>(null);
-  const observerRef: MutableRefObject<IntersectionObserver | null> = useRef(
-    null
-  );
-  const timeoutRef: MutableRefObject<NodeJS.Timeout | null> = useRef(null);
+  const observerRef = useRef<IntersectionObserver | null>(null);
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
   const erroredRef = useRef<boolean>(false);
   let numThreshold = threshold;
 
